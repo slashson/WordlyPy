bc91a98ab01b46379d0faaeb36167b2e38f01914
volodymyrklevtsov <vovikoo.jpeg@gmail.com>
2025-05-18 11:05:10 +0200

    start making a UI (backup)

--- gui.py
+++ gui.py
@@ -1,43 +1,59 @@
 import customtkinter as ctk
+from customtkinter import CTkFrame
+
 from main import match, load_word, random_choise
 import random
 
 
-def check_guess():
-    pass
+class WorldlyApp:
+    def __init__(self, screen):
+        self.screen = screen
+        self.screen.geometry('550x750')
+        self.screen.title('WorldlyPY')
+        self.screen.configure(fg_color='#211F20')
+        self.secret_word = None
 
 
-def user_guess():
-    pass
+        self.play_screen()
+
+    def play_screen(self):
+        self.clear_screen()
+
+        self.choose_secret_word(secret_word=random_choise())
+        self.frame = ctk.CTkFrame(self.screen, fg_color='#211F20')
+        self.frame.place(relx=0.5, rely=0.5, anchor='center')
+        # ctk.CTkLabel(frame, text="Register", width=50, height=50, font=("SF Pro", 25, "bold")).pack(padx=130, pady=20)
+        self.playground(screen=self.screen)
+
+    def clear_screen(self):
+        for widget in self.screen.winfo_children():
+            widget.destroy()
+    def choose_secret_word(self, secret_word):
+        self.secret_word = secret_word
+        print(f'The secret word : {self.secret_word}')
 
 
+# UI Shit
+    def playground(self, screen):
+        self.screen = screen
+        entries = [[None for _ in range(4)] for _ in range(5)]
 
-results = ['' for i in range(5)]
-
-# Настройка окна
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
-
-app = ctk.CTk()
-app.geometry("400x600")
-app.title("Wordle GUI")
-
-# Ввод
-entry = ctk.CTkEntry(app, placeholder_text="Введите слово")
-entry.pack(pady=20)
-
-# Результаты
-result_label = ctk.CTkLabel(app, text="")
-result_label.pack(pady=10)
-
-button = ctk.CTkButton(app, text="Проверить", command=check_guess)
-button.pack(pady=10)
-
-
-
-app.mainloop()
-
-# Загрузка слов
-load_word()
-print(clean_lines)
-random_choise()
+        for row in range(5):
+            for col in range(4):
+                entry = ctk.CTkButton(self.frame,
+                                      width=87,
+                                      height=87,
+                                      anchor='center',
+                                      fg_color='#211F20',
+                                      hover_color='#211F20',
+                                      border_color='#4D4D4D',
+                                      border_width=4,
+                                      corner_radius=13,
+                                      text='')
+                entry.grid(row=row, column=col, padx=5, pady=5)
+                entries[row][col] = entry
+'''Add Lose Screen, restart button'''
+'''Add Win Screen, new game button'''
+root = ctk.CTk()
+app = WorldlyApp(root)
+root.mainloop()
