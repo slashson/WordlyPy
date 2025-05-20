3ba90ce3fa18c63c2c3456be7f374616559dab6d
volodymyrklevtsov <vovikoo.jpeg@gmail.com>
2025-05-21 00:44:05 +0200

    edit class made it ready to realis alpha

--- gui.py
+++ gui.py
@@ -1,9 +1,8 @@
 import customtkinter as ctk
-from customtkinter import CTkFrame
-
-from main import match, load_word, random_choise
+from keyboard import Keyboard
+from main import match, load_word, random_choise, results
 import random
-
+from keyboard import Keyboard
 
 class WorldlyApp:
     def __init__(self, screen):
@@ -12,7 +11,11 @@ class WorldlyApp:
         self.screen.title('WorldlyPY')
         self.screen.configure(fg_color='#211F20')
         self.secret_word = None
+        self.current_row = 0
+        self.current_coulmn = 0
 
+        self.user_guesses = None
+        self.temp_text = []
 
         self.play_screen()
 
@@ -32,11 +35,10 @@ class WorldlyApp:
         self.secret_word = secret_word
         print(f'The secret word : {self.secret_word}')
 
-
 # UI Shit
     def playground(self, screen):
         self.screen = screen
-        entries = [[None for _ in range(4)] for _ in range(5)]
+        self.entries = [[None for _ in range(4)] for _ in range(5)]
 
         for row in range(5):
             for col in range(4):
@@ -49,11 +51,41 @@ class WorldlyApp:
                                       border_color='#4D4D4D',
                                       border_width=4,
                                       corner_radius=14,
-                                      text='')
+                                      text='',
+                                      state="disabled")
                 entry.grid(row=row, column=col, padx=2, pady=2)
-                entries[row][col] = entry
-'''Add Lose Screen, restart button'''
-'''Add Win Screen, new game button'''
+                self.entries[row][col] = entry
+
+        self.keyboard = Keyboard(self.screen,0.5, 0.88, on_type=self.handle_typing)
+
+    def handle_typing(self, word):
+        word = word.upper()
+
+        for i, letter in enumerate(word):
+            self.entries[self.current_row][i].configure(text=letter,
+                                                        font=('SF Pro', 20))
+
+        if len(word) == 4:
+            result = match(list(word.lower()), self.secret_word)
+
+            for i in range(4):
+                color = result[i].strip()
+                if color == 'g':
+                    bg = '#00A878'
+                elif color == 'y':
+                    bg = '#FFD447'
+                else:
+                    bg = '#3A3A3C'
+
+                self.entries[self.current_row][i].configure(fg_color=bg)
+
+            self.keyboard.text = []
+
+            self.current_row += 1
+
+'''как сделать изменение квадратиков в зависимости от results'''
+'''Add Lose Screen, show guessed word, restart button'''
+'''Add Win Screen, new game button, auto restart in 5 sec'''
 root = ctk.CTk()
 app = WorldlyApp(root)
 root.mainloop()
